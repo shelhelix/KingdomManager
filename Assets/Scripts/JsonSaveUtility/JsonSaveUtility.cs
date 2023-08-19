6451e53bcd5ffc2811470fa82d7bb6ff21ed3b73
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2023-08-19 19:07:13 +0300

    added NewtonSoftJson package

---
+++ Assets/Scripts/JsonSaveUtility/JsonSaveUtility.cs
@@ -0,0 +1,8 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace JsonSaveUtility {
+	public class JsonSaveUtility {
+		// TODO: write helpers for loading from JSON / saving to JSON
+		// TODO: ver2 - add save migration support
+	}
+}
