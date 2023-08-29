a942a61fa504aed06ce8837af52fc855c318e2d0
alexandr.shelekhin <alexandr.shelekhin@matryoshka.com>
2023-08-29 14:34:34 +0300

    moved package from Assets to Packages

--- Assets/!Thirdparty/com.ultrashel.gamecomponentattributes@e43724efe7/Attributes/EnumNotEqualsAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace GameComponentAttributes.Attributes {
-	public class EnumNotEqualsAttribute : BaseGameComponentAttribute {
-		public readonly int EnumIntValue;
-		
-		public EnumNotEqualsAttribute(int enumIntValue, bool checkInPrefab = true) : base(checkInPrefab) {
-			EnumIntValue = enumIntValue;
-		}
-	}
-}
